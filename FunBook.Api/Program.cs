c0f3e819addceced71feb6d2c20cc2eeb08144b1
Antonio <antoniorivera.cs18@gmail.com>
2026-06-14 12:51:01 -0400

    Configured use of controllers. Was able to test with swagger stub and dummy data execution

--- FunBook.Api/Program.cs
+++ FunBook.Api/Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
